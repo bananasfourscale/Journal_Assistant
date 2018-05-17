7379ffd9ee1ee0b232f511969a1419d7c3ad1223
bananasfourscale <lindbergna@gmail.com>
2018-05-16 22:40:58 -0700

    creating initial commit on master branch to create the repository.

---
+++ Journal/Journal.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+"""Class Representing a Journal which holds different types of JournalEntires"""
+
+
+class Journal:
+    """
+    Class Representing the Journal of a player. Can contain any number of different entries specific to the
+        journey of the player, and handles storage and retrieval of entries by saving pertinent data to a file
+        upon request, and on successful exit of program.
+
+    Attributes:
+        saved(bool): Flag indicating if the journal has been saved since the most recent update of any of its entries
+        quest_entries(dict): Dictionary of all journal entries which fall into the Quest classification,
+            mapped with entry names as keys and entry references as values.
+        map_markers(dict): Dictionary of all journal entries which fall into the MapMarker classification,
+            mapped with entry names as keys and entry references as values.
+        character_bios(dict): Dictionary of all journal entries which fall in the the CharacterBio classification,
+            mapped with entry names as keys and entry references as values.
+    """
+
+    def __init__(self):
+        """
+        Initialize a journal instance to be used by either a DM or Player Character
+        """
+        self.saved = False
+        self.quest_entries = {}
+        self.map_markers = {}
+        self.character_bios = {}
+
+    def add_quest_entry(self, entry):
+        """
+        Add a new quest entry to the journal. Entries can be of a few different types including,
+            MainQuest, MajorQuest, FactionQuest, SideQuest, MinorQuest, Task
+
+        :param entry: any class which inherits from the base JournalEntry class and falls into the Quest
+            classification.
+        :return: None
+        """
+
+        self.quest_entries[entry.name] = entry
+        self.saved = False
+
+    def update_quest_entry(self, entry):
+        """
+        Call the update method for the given entry
+
+        :param entry:
+        :return:
+        """
+
+    def add_map_marker(self, marker):
+        """
+        Add a new map marker to the journal. Map markers can be one a few different types including,
+            Capital, City, Village, WildernessSite
+
+        :param marker: any class which inherits from the base JournalEntry class and falls in the MapMarker
+            classification
+        :return: None
+        """
+
+        self.map_markers[marker.name] = marker
+        self.saved = False
+
+    def add_character_bio(self, bio):
+        """
+        Add a new bio to the journal. Bios are commonly character biographies of characters
+                that are met in the players travels.
+
+        :param bio: any class which inherits from the base JournalEntry class and falls into the CharacterBio
+            classification.
+        :return: None
+        """
+
+        self.character_bios[bio.name] = bio
+        self.saved = False
+
+    def add_bestiary_entry(self, entry):
+        """
+        Add a new bestiary entry to the journal. Bestiary entries are used to chronicle the stats and abilities of
+            creatures that are encountered by the player on their journey.
+
+        :param entry: any class which inherits from the base JournalEntry class and falls in the Bestiary
+            classification.
+        :return: None
+        """
---
+++ Journal/JournalEntry.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""Base Class representing all types of Journal Entires that can be added to the journal"""
+
+
+class JournalEntry:
+    """
+    Represents a journal entry which is the base class used by many other types of entires that can
+        be added to the Journal
+
+    Attributes:
+        name(str): string which gives a characteristic name to help the user identify the entry
+        updated(bool): boolean indicator for determining if the entry has been updated recently
+            and should be saved upon exit or call to save.
+    """
+
+    def __init__(self, name):
+        """
+        Initialize a class instance and set up any default instance variables
+        """
+
+        self.name = name
+        self.updated = True
+
+    def update_entry(self):
+        """
+        Function meant to be overwritten by the inheriting classes, which will take in characteristic
+            info and use that to update the specific entry data.
+
+        :return: None
+        """
+        self.updated = True
+
+    def save_entry(self):
+        """
+        Function meant to be overwritten by inheriting classes, which will collect all important entry
+            details and place them into a dictionary so that they can be saved. Will also set the updated
+            flag to false to inform the journal that this entry has been saved.
+
+        :return: dict - dictionary mapping named entry characteristics with details about the set
+            characteristic
+        """
+        self.updated = False
+
+    def read_entry_log(self, log):
+        """
+
+        :param log: dictionary mapping entry details with the data structure used to hold that detail type.
+            Logs are read for each entry upon opening a saved Journal.
+        :return: None
+        """
