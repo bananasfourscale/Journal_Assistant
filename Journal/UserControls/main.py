13bc007bc3e1d9d6d5ca7506da675b79d09aefd4
Unknown <lindbergna@gmail.com>
2018-07-22 13:07:32 -0700

    Got gui working
    gui base is working based off an online example, will now customize to fit this application

--- Journal/UserControls/main.py
+++ Journal/UserControls/main.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 from Journal.Journal import Journal
 from Journal.FileSystem.JournalFileLogger import JournalFileLogger
 from Journal.Quest.MainQuest import MainQuest
+import tkinter as tk
+from Journal.UserControls.Application import Application
 
 if __name__ == '__main__':
-    PlayerJournal = Journal()
+
+    journal = Journal()
+    file_system = JournalFileLogger()
     # todo: move all this shit to a simple gui framework.
     log_file = input("Please Enter the Full Path of the Journal File you Would Like to Load")
-    JournalFileLogger.set_log_file(log_file=log_file)
+    file_system.set_log_file(file_name=log_file)
+    app = Application()
+    app.mainloop()
+    exit(0)
+
